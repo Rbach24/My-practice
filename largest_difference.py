a4297bef0b067c71e47bddbf2f25d5a3ed419b3f
Rbach24 <96384502+Rbach24@users.noreply.github.com>
2021-12-30 23:57:39 -0500

    Add files via upload

---
+++ largest_difference.py
@@ -0,0 +1,15 @@
+n = int(input("Enter number of elements: "))
+a = []
+for i in range(0,n):
+	x = int(input("Enter number for your first list: "))
+	a.append(x)
+x = None
+y = a[2]
+for i in range (0,n):
+	if a[i] > x :
+		x = a[i]
+	if a[i] < y :
+		y = a[i]
+print(x-y)
+
+
